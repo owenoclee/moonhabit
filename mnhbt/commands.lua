0cd9a599afc427a4e9a7deda22740eaaaf45f9b9
owenoclee <owenoclee@users.noreply.github.com>
2017-04-07 17:36:38 +0100

    Remove habit global variable from commands.lua

--- mnhbt/commands.lua
+++ mnhbt/commands.lua
@@ -1,21 +1,22 @@
-local consts = require "consts"
+local consts = require "mnhbt.consts"
+local habits = {}
 
 local COMMANDS = {
 	add = {
 		arity = 1,
-		run = function(name)
+		run = function(habits, name)
 			habits[name] = habits[name] or { meta = { count = 0, streak = 0 } }
 		end
 	},
 	rm = {
 		arity = 1,
-		run = function(name)
+		run = function(habits, name)
 			habits[name] = nil
 		end
 	},
 	check = {
 		arity = 1,
-		run = function(name)
+		run = function(habits, name)
 			if not habits[name][consts.TODAY] then
 				habits[name][consts.TODAY] = {}
 				habits[name].meta.count = habits[name].meta.count + 1
@@ -29,7 +30,7 @@ local COMMANDS = {
 	},
 	uncheck = {
 		arity = 1,
-		run = function(name)
+		run = function(habits, name)
 			if habits[name][consts.TODAY] then
 				habits[name][consts.TODAY] = nil
 				habits[name].meta.count = habits[name].meta.count - 1
@@ -39,7 +40,7 @@ local COMMANDS = {
 	},
 	display = {
 		arity = 1,
-		run = function(name)
+		run = function(habits, name)
 			for k, _ in pairs(habits[name]) do
 				print(k)
 			end
@@ -47,7 +48,7 @@ local COMMANDS = {
 	},
 	ls = {
 		arity = 0,
-		run = function()
+		run = function(habits)
 			for k, _ in pairs(habits) do
 				print(string.format("%s: %d day streak. %d recorded events.", k,
 						habits[k].meta.streak, habits[k].meta.count))
@@ -76,13 +77,13 @@ setmetatable(COMMANDS, {
 })
 
 return {
-	tryRun = function(command, args)
+	tryRun = function(command, args, habits)
 		if #args > COMMANDS[command].arity then
 			return false, "Too many arguments"
 		elseif #args < COMMANDS[command].arity then
 			return false, "Insufficient arguments"
 		end
-		COMMANDS[command].run(unpack(args))
+		COMMANDS[command].run(habits, unpack(args))
 		return true
 	end
 }
