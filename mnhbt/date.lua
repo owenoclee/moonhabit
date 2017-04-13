7ef099017b587db4fb48a6e2878b78251af94894
owenoclee <owenoclee@users.noreply.github.com>
2017-04-13 20:59:10 +0100

    Fix date.lua not being able to access its own function

--- mnhbt/date.lua
+++ mnhbt/date.lua
@@ -1,10 +1,12 @@
-return {
-	dateDaysFromNow = function(days)
-		local date = os.date("*t")
-		date.day = date.day + days
-		return os.date("%Y-%m-%d", os.time(date))
-	end,
-	today = function() return dateDaysFromNow(0) end
-	yesterday = function() return dateDaysFromNow(-1) end
-}
+local date = {}
 
+date.dateDaysFromNow = function(days)
+	local date = os.date("*t")
+	date.day = date.day + days
+	return os.date("%Y-%m-%d", os.time(date))
+end
+
+date.today = function() return date.dateDaysFromNow(0) end
+date.yesterday = function() return date.dateDaysFromNow(-1) end
+
+return date
